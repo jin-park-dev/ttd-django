36e9fa6d8f22122ffba5e5a32bd1482273d0c190
Jin Park <JinPark.Han@gmail.com>
2018-07-11 07:06:25 +0100

    Refactored checking rows

--- functional_test.py
+++ functional_test.py
@@ -10,6 +10,11 @@ class NewVistortest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+        
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_litle_and_retrieve_it_later(self):
         # Edith checks out to-do page
@@ -36,12 +41,7 @@ class NewVistortest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were: \n{table.text}"
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -53,13 +53,9 @@ class NewVistortest(unittest.TestCase):
 
         # The page updates again, and now shows both items on her list
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
-        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
         self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
