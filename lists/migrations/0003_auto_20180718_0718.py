b091e1da5738fbaa1ca52454cd825441526dcc24
oneGene <jinpark.han@gmail.com>
2018-08-06 06:13:58 +0100

    use new form in home_page, simplify tests. NB break stuff

--- lists/migrations/0003_auto_20180718_0718.py
+++ lists/migrations/0003_auto_20180718_0718.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('lists', '0002_item_text'),
+        ('lists', '0002_text'),
     ]
 
     operations = [
