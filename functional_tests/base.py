81b25e86389762740bd06f0711c7235301f40586
oneGene <jinpark.han@gmail.com>
2018-08-15 04:53:23 +0100

    Fix missing wait causing wait_for_row_in_list_table to fail

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -8,6 +8,19 @@ import time
 MAX_WAIT = 10
 
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -19,22 +32,11 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
 
     @wait
     def wait_for(self, fn):
